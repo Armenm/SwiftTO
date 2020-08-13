33fedfd42f2f5ba8db79ff9b91594431f9e0f418
Armen Mkrtchian <armen.mkrtchian@gmail.com>
2020-08-13 13:53:36 -0400

    Added List and OutlineGroup

--- Sandbox/Sandbox/ContentView.swift
+++ Sandbox/Sandbox/ContentView.swift
@@ -5,29 +5,35 @@
 //  Created by Armen Mkrtchyan on 2020-08-13.
 //
 
-import MapKit
 import SwiftUI
 
-struct City: Identifiable {
-    var id: String { name }
-    let name: String
-    let location: CLLocationCoordinate2D
-    
-    static let london = City(name: "London", location: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275))
-    static let paris = City(name: "Paris", location: CLLocationCoordinate2D(latitude: 48.864716, longitude: 2.349014))
+struct Person: Identifiable {
+    let id = UUID()
+    var name: String
+    var children: [Person]?
 }
 
 struct ContentView: View {
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
-    let annotations = [City.london, City.paris]
+    var people: [Person] {
+        let sophie = Person(name: "Sophie")
+        let lottie = Person(name: "Lottie")
+        let john = Person(name: "John")
+        let paul = Person(name: "Paul", children: [sophie, lottie])
+        let andrew = Person(name: "Andrew", children: [john])
+        let terry = Person(name: "Terry", children: [paul, andrew])
+        
+        return [terry]
+    }
     
     var body: some View {
-        Map(coordinateRegion: $region, annotationItems: annotations) {
-            annotation in
-            MapAnnotation(coordinate: annotation.location, anchorPoint: CGPoint(x: 0.5, y: 1)) {
-                Image(systemName: "mappin")
-                    .imageScale(.large)
-                    .foregroundColor(.red)
+        List(people, children: \.children) { person in
+            if person.children != nil {
+                Image(systemName: "person")
+                Text(person.name)
+                    .font(.headline)
+            } else {
+                Image(systemName: "person.circle")
+                Text(person.name)
             }
         }
     }
